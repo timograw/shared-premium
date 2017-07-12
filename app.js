72e8a30016be2e20a1a1ed64a0fff186ea2687c0
Timo Graw <tigraw@uos.de>
2017-07-12 23:59:18 +0200

    using app-layout

--- app.js
+++ app.js
@@ -17,12 +17,30 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+var options = {
+  dotfiles: 'ignore',
+  etag: true,
+  extensions: ['htm', 'html'],
+  index: 'index.html',
+  lastModified: true,
+  maxAge: '1d',
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+    res.header('Cache-Control', 'public, max-age=1d');
+  }
+};
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/', express.static(path.join(__dirname, 'client')));
+app.use('/', express.static(path.join(__dirname, 'client'), options));
 app.use('/api', api);
+
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, 'client/index.html'), options);
+});
 
 // app.use('/', index);
 // app.use('/users', users);

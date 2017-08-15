d0c2dedc9fb4f6e91712b777520ee2169c14b89f
Timo Graw <tigraw@uos.de>
2017-08-15 23:44:39 +0200

    Made adduser interactive

--- adduser.js
+++ adduser.js
@@ -1,30 +1,42 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/shared-premium');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
 
 var password = require('password-hash-and-salt');
 
-var db = require('mongoose').connection;
-db.on('error', console.error.bind(console, 'connection error:'));
 
-var User = require('./api/models/userModel');
 
-var user = new User({ 
-    username: 'test',
-    is_admin: true
- });
 
-console.log(user.username);
+rl.question("Username: ", (username) => {
+    rl.question("Password: ", (pass) => {
+        var mongoose = require('mongoose');
+        mongoose.connect('mongodb://localhost/shared-premium');
+        var db = require('mongoose').connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
 
-password('test').hash(function (error, hash) {
-    if (error) throw error;
-    user.password_hash = hash;
-    user.save(function (err) {
-        if (err)  throw err;
-        console.log('saved');
-        process.exit();
-    });
+        var User = require('./api/models/userModel');
 
-    // process.exit();
+        var user = new User({ 
+            username: username,
+            is_admin: false
+        });
 
-});
+        password(pass).hash(function (error, hash) {
+            if (error) throw error;
+            user.password_hash = hash;
+            user.save(function (err) {
+                if (err)  throw err;
+                console.log('saved');
+                process.exit();
+            });
+        });
+    })
+})
+
+
 
